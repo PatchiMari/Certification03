4b3efd8c2c448f707c3ea3f976423968393544b6
Maria Vorontsova <miss.konyaeva@yandex.ru>
2024-01-29 18:49:00 +0300

    Формирование нового массива

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод массива строк с клавиатуры
+        Console.WriteLine("Введите элементы массива (через запятую):");
+        string[] input = Console.ReadLine().Split(',');
+
+        // Формирование нового массива из строк, длина которых меньше или равна 3 символам
+        string[] output = GetStrings(input);
+
+        // Вывод нового массива
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < output.Length; i++)
+        {
+            Console.WriteLine(output[i]);
+        }
+
+    }
+
+    static string[] GetStrings(string[] input)
